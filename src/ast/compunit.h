717dacd832077cb9767ad1c312fa01315d621b82
pache <patchouli0416@163.com>
2023-09-14 14:58:51 +0800

    compunit ast

--- src/ast/compunit.h
+++ src/ast/compunit.h
@@ -2,9 +2,9 @@
 #define COMPUNIT_H
 
 #include "ast.h"
-#include "ast/function.h"
 #include "class.h"
 #include "expression.h"
+#include "function.h"
 #include "statement.h"
 #include "type.h"
 #include <algorithm>
@@ -12,85 +12,30 @@
 #include <memory>
 #include <string>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 namespace pache {
+class class_ast;
 class compunit_ast : public base_ast {
 public:
   explicit compunit_ast() {}
-  /* virtual llvm::Value *codegen() override {
-    for (auto &stmt : m_var_table) {
-
-      if (stmt != nullptr) {
-        auto val = stmt->codegen();
-        if (val) {
-          //   val->print(llvm::errs());
-        }
-        std::cout << "comp unit.\n";
-      }
-    }
-    return nullptr;
-  } // TODO */
-  /*   virtual void insert_dec(stmt_ast *let) {
-      let->set_father(get_father());
-      m_var_table.push_back(std::unique_ptr<stmt_ast>(let));
-    }
-    void insert_dec(func_ast *func) { m_func_asts.push_back(func); }
-    void insert_dec(variable_ast *func) {
-      m_var_table.push_back(std::unique_ptr<variable_ast>(func));
-      func->set_father(get_father());
-    } */
-  /*   virtual void insert_dec(std::string &&func_name, func_ast *func) {
-      m_func_table.emplace(std::move(func_name), func);
-      func->set_father(get_father());
-    } */
-  // virtual void insert_dec(std::string &&name, variable_ast *dec) {
-  //   dec_name.emplace_back(std::move(name), dec);
-  // }
-
-  /*   virtual variable_ast *find_dec(const std::string &name) const override {
-      auto beg = this->m_symbol_table.find(name);
-      if (beg != this->m_symbol_table.end()) {
-        return beg->second;
-      } else if (get_father() != nullptr) {
-        return get_father()->find_dec(name);
-      } else {
-        return nullptr;
-      }
-    } */
-
   void insert_class_def(std::unique_ptr<class_ast> &&p) {
-    p->set_father(get_father());
+    p->set_father(this->get_father());
     class_def.push_back(std::move(p));
   }
 
-  /*   class_ast *find_class_def(std::string const &iden) {
-      return class_def.find(iden) != class_def.end()
-                 ? class_def.find(iden)->second
-                 : nullptr;
-    } */
-  // virtual llvm::AllocaInst *
-  // find_named_Value(std::string const &name) const override;
-  // virtual void insert_named_Value(std::string const &name,
-  //                                 llvm::AllocaInst *val) override;
-  /*   virtual func_ast *ADL(std::string const &name,
-                          std::vector<exp_ast *> const &args) {
-      /*     auto [begin, end] = m_func_table.equal_range(name);
-          std::vector<type_ast const *> args_type;
-          for (auto exp : args) {
-            args_type.emplace_back(exp->get_type());
-          }
-          for (; begin != end; ++begin) {
-            auto beg = args_type.begin();
-          loop1:
-            if (beg == args_type.end()) {
-              goto break1;
-            }
-            auto [end1, end2] = std::mismatch(args_type.begin(),
-     args_type.end(), beg->second->) break1:
-          } ****   //
-  return nullptr; // TODO
-} */
+  void insert_func_def(std::unique_ptr<func_ast> &&p) {
+    m_func_asts.emplace_back(std::move(p));
+  }
+
+  void insert_dec(std::unique_ptr<base_ast> &&p) {
+    m_var_table.emplace_back(std::move(p));
+  }
+  std::vector<std::unique_ptr<func_ast>> const &get_funcs_ast() const {
+    return m_func_asts;
+  }
+  virtual ~compunit_ast() = default;
 
 private:
   std::vector<std::unique_ptr<base_ast>> m_var_table;
