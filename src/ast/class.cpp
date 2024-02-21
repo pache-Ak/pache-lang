942a566e9218147b7ec762558fd4e52e4fc9f03e
pache <patchouli0416@163.com>
2024-02-21 19:23:38 +0800

    add class name in class_ast print.

--- src/ast/class.cpp
+++ src/ast/class.cpp
@@ -7,6 +7,7 @@ pache::class_ast::class_ast(std::string && name, class_body &&body)
 }
 void pache::class_ast::print() const {
   std::cout << "class_ast:\n"
+            << "m_name:" << m_name << "\n"
             << "inner_class_def:{\n";
   for (auto const &ast : m_body.inner_class_def) {
     ast->print();
